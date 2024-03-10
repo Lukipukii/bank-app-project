810e9db586f22a16281b9491dc6c42428924ff53
Lucas Colban <lukipuki.git@gmail.com>
2024-03-10 23:20:48 +0100

    Añadido css del proyecto original y su estructura

--- bank-app/src/App.js
+++ bank-app/src/App.js
@@ -1,25 +1,103 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react'
+import './App.css'
+import Welcome from './Welcome/Welcome'
+import Login from './Login/Login'
+import Summary from './Summary/Summary'
+import Movements from './Movements/Movements'
+import Balance from './Balance/Balance'
+import Transfer from './Transfer/Transfer'
+import AccountData from './AccountData/AccountData'
+import Withdraw from './Withdraw/Withdraw'
+import Logout from './Logout/Logout'
+import Deposit from './Deposit/Deposit'
 
 function App() {
+  const [account, setAccount] = useState({})
+  const [token, setToken] = useState()
+  const { movements = [], owner: user = '' } = account
+
+  useEffect(() => {
+    // Función para obtener los datos del usuario
+    const fetchUserData = () => {
+      if (!token) return // No hay token, no se puede obtener datos
+
+      fetch('http://localhost:3000/user?token=' + token)
+        .then((response) => {
+          if (!response.ok) {
+            throw new Error('Network response was not ok')
+          }
+          return response.json()
+        })
+        .then((data) => {
+          setAccount(data.account)
+        })
+        .catch((error) => {
+          console.error('There was a problem with the fetch operation:', error)
+        })
+    }
+
+    // Obtener los datos del usuario inicialmente
+    fetchUserData()
+
+    // Configurar una función para obtener los datos cuando se detecta una transferencia
+    const handleTransfer = () => {
+      fetchUserData()
+    }
+
+    // Agregar un event listener para manejar las transferencias
+    document.addEventListener('transfer', handleTransfer)
+
+    // Limpiar el event listener cuando el componente se desmonte
+    return () => {
+      document.removeEventListener('transfer', handleTransfer)
+    }
+  }, [token]) // Se ejecuta solo cuando cambia el token
+
+  const handleLogin = (user, pin) => {
+    const url = `http://localhost:3000/login?username=${user}&pin=${pin}`
+
+    fetch(url)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok')
+        }
+        return response.json()
+      })
+      .then((data) => {
+        setToken(data.token)
+        setAccount(data.account)
+        console.log(data)
+      })
+      .catch((error) => {
+        console.error('There was a problem with the fetch operation:', error)
+      })
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <nav>
+        <Welcome user={user} />
+        <Login onLogin={handleLogin} />
+      </nav>
+      {user && (
+        <main className="app">
+          <Balance movements={movements} />
+          <Movements movements={movements} />
+          <Summary movements={movements} />
+
+          <Transfer token={token} />
+          <Withdraw token={token} account={account.numberAccount} />
+          <Deposit token={token} />
+
+          <p className="logout-timer">
+            You will be logged out in <span className="timer">05:00</span>
+          </p>
+
+          <Logout token={token} />
+        </main>
+      )}
+    </>
+  )
 }
 
-export default App;
+export default App
